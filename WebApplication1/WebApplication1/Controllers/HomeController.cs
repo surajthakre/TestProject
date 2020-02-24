b7c1765a09e993e8c568c31b2323ca82c6e968a6
suraj d thakre <suraj.thakre@perficient.com>
2020-02-24 14:41:22 +0530

    Home controller changes

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page3.";
+            ViewBag.Message = "Your application description page.";
 
             return View(); 
         }
